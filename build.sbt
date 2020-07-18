5f49e7b0bf0488acad93bde50db2bfe7f711bd6c
keynmol <keynmol@gmail.com>
2020-07-18 10:51:37 +0100

    experiment with bundler and MIDI

--- build.sbt
+++ build.sbt
@@ -1,10 +1,15 @@
 enablePlugins(ScalaJSPlugin)
-
+enablePlugins(ScalablyTypedConverterPlugin)
 name := "french-connection"
 scalaVersion := "2.13.1" // or any other Scala version >= 2.11.12
 
 libraryDependencies += "com.raquo" %%% "laminar" % "0.9.2"
 libraryDependencies += "org.typelevel" %%% "cats-effect" % "2.1.4"
+
+Compile / npmDependencies ++= Seq(
+  "webmidi" -> "2.5.1",
+  /* "@types/webmidi" -> "2.0.4" */
+)
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,14 @@ import cats.effect._
 import scala.concurrent.Future
 import com.raquo.airstream.signal.Signal
 
+import typings.webmidi
+import typings.std.global.AudioContext
+import typings.std.global.navigator
+import scala.concurrent.ExecutionContext
+import org.scalajs.dom.raw.OscillatorNode
+
 object Main {
+  implicit val cs = IO.contextShift(ExecutionContext.global)
   def main(args: Array[String]): Unit = {
 
     val customAction = IO {
@@ -15,17 +22,55 @@ object Main {
 
     }
 
+    object ManagedOscillator {
+      private val acquire = IO {
+
+        val context = new AudioContext
+
+        val oscillator = context.createOscillator()
+
+        oscillator.frequency.setValueAtTime(110.0, 0)
+        val envelope = context.createGain()
+        oscillator.connect(envelope)
+        envelope.connect(context.destination)
+        envelope.gain.value = 0.0
+        oscillator.start(0)
+
+        (oscillator, context)
+      }
+
+      private val dispose =
+        (oscillator: OscillatorNode, context: AudioContext) =>
+          IO(oscillator.stop()) *>
+            IO.fromFuture(IO(context.close().toFuture))
+
+      val create = Resource.make(acquire)(dispose.tupled)
+    }
+
+    val printWebMidiOutputs = IO(println(webmidi.mod.default.outputs.toList))
+
+    val playSomething = IO {
+      val midiOutput = webmidi.mod.default.outputs.toList.headOption
+
+      midiOutput.map { o =>
+        println(o.name)
+        o.playNote("C3")
+      }
+    }
+
     def makeButton(action: IO[_]) = {
       val observer = Observer.apply[Any](_ => action.unsafeRunAsyncAndForget())
       div(button("Play", onClick --> observer))
     }
 
-    render(
-      dom.document.querySelector("#appContainer"),
-      div(
-        div(idAttr := "test"),
-        makeButton(customAction)
+    webmidi.mod.default.enable { err =>
+      render(
+        dom.document.querySelector("#appContainer"),
+        div(
+          div(idAttr := "test"),
+          makeButton(ManagedOscillator.create.use(_ => playSomething.void))
+        )
       )
-    )
+    }
   }
 }
